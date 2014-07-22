c544c228ad49d72ba44bc214b1b747586e5efc8e
msalli <alli.cernoch@gmail.com>
2014-07-22 16:16:05 -0700

    App file

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
-var express = require("express");
+var express = require("express"),
+    request = require('request');
 
 var app = express();
 
 app.get('/', function(req, res){
   res.render('index.ejs');
 });
+
+app.get('/search', function(req, res) {
+  var query = req.query.searchTerm;
+  // res.send("search page: " + query);
+  var url = "http://www.omdbapi.com/?s=" + query;
+
+  request(url, function (error, response, body) {
+    if (!error) { 
+      var data = JSON.parse(body);
+      res.render('results.ejs', {movieList: data.Search || []});
+    } 
+  });
+
+});
+
+
 
 app.listen(3000);
